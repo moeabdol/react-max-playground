46cb35fbdb45f0b3c54f1df16c2df1abcd76d6a2
moeabdol <mohd.a.saed@gmail.com>
2018-01-09 17:21:13 +0300

    Installing radium and configure button hover effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -45,7 +46,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -63,6 +68,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
     }
 
     let classes = [];
@@ -82,4 +91,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
