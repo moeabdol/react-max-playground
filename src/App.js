6b5e60300477e04b51b91c2f035e114b5a537593
moeabdol <mohd.a.saed@gmail.com>
2018-01-09 16:53:20 +0300

    Manipulate css classes dynamically

--- src/App.js
+++ src/App.js
@@ -40,7 +40,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -61,12 +62,17 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
     }
+
+    let classes = [];
+    if (this.state.persons.length <= 2) classes.push('red');
+    if (this.state.persons.length <= 1) classes.push('bold');
 
     return (
       <div className="App">
         <h1>Hi, I&apos;m a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
