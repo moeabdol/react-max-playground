e881335bfacb1ee33ed98e17ba700940c4065669
moeabdol <mohd.a.saed@gmail.com>
2018-01-09 16:31:47 +0300

    Implement input changed handler

--- src/App.js
+++ src/App.js
@@ -13,14 +13,15 @@ class App extends Component {
     showPersons: false
   };
 
-  nameChangedHandler = event => {
-    this.setState({
-      persons: [
-        { name: 'moe', age: 35 },
-        { name: event.target.value, age: 36 },
-        { name: 'doe', age: 37 }
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({ persons });
   }
 
   deletePersonHandler = (index) => {
@@ -53,9 +54,10 @@ class App extends Component {
           {this.state.persons.map((person, index) => {
             return <Person
               click={() => this.deletePersonHandler(index)}
-              key={person.name}
+              key={person.id}
               name={person.name}
-              age={person.id} />;
+              age={person.age}
+              changed={event => this.nameChangedHandler(event, person.id)} />;
           })}
         </div>
       );
