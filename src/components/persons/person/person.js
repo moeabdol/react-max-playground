7aeed608f434bfe55e91f8ebd47db4c52997b174
moeabdol <mohd.a.saed@gmail.com>
2018-01-10 13:22:22 +0300

    Change Persons and Person to statefull components

--- src/components/persons/person/person.js
+++ src/components/persons/person/person.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './person.css';
 
-const person = props => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I&apos;m {props.name} and I&apos;m {props.age} years old</p>
-      {props.children ? <p>{props.children}</p> : false}
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
+class Person extends Component {
+  render() {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>I&apos;m {this.props.name} and I&apos;m {this.props.age} years old</p>
+        {this.props.children ? <p>{this.props.children}</p> : false}
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </div>
+    );
+  }
+}
 
-export default person;
+export default Person;
